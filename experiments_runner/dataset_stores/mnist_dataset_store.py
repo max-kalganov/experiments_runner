39089833e3652f7c9a7f4c150adead6fb70fab7f
max-kalganov <maksim.kalganov@gmail.com>
2024-07-13 22:29:48 +0300

    add basic mnist dataset store

--- experiments_runner/dataset_stores/mnist_dataset_store.py
+++ experiments_runner/dataset_stores/mnist_dataset_store.py
@@ -1,5 +1,28 @@
+import typing as tp
+from keras import datasets
+
 from experiments_runner.dataset_stores.dataset_store import DatasetStore
 
 
-class MNISTDatasetStore(DatasetStore):
-    pass
+class MNISTTrainDatasetStore(DatasetStore):
+    def __init__(self):
+        super().__init__(name='mnist_train')
+
+        (x_train, y_train), _ = datasets.mnist.load_data()
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def yield_samples(self) -> tp.Iterable[tp.Tuple[tp.Any, tp.Any]]:
+        yield from zip(self.x_train, self.y_train)
+
+
+class MNISTTestDatasetStore(DatasetStore):
+    def __init__(self):
+        super().__init__(name='mnist_test')
+
+        _, (x_test, y_test) = datasets.mnist.load_data()
+        self.x_test = x_test
+        self.y_test = y_test
+
+    def yield_samples(self) -> tp.Iterable[tp.Tuple[tp.Any, tp.Any]]:
+        yield from zip(self.x_test, self.y_test)
